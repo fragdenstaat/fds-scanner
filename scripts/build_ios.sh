f9b9327a6b56c4b15954db4dd934aed5fe8d5396
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-03-04 20:46:37 +0100

    Source env file when running build script

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-. .env
 set -ex
 
-
+set -a && source .env && set +a
 pnpm tauri icon public/fds-scanner.png --ios-color "#fff"
 pnpm tauri ios build --export-method app-store-connect
 xcrun altool --upload-app --type ios --file src-tauri/gen/apple/build/arm64/fds-scanner.ipa --apiKey $APPLE_API_KEY_ID --apiIssuer $APPLE_API_ISSUER
