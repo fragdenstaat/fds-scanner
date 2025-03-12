0dae5b1fdccf73c392ebc00af1b67e62444eeec0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-03-12 12:19:07 +0100

    Add tauri opener plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -143,6 +143,7 @@ pub fn run() {
     ));
 
     let mut builder = tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
