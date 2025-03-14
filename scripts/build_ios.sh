bde3b79b0b744431bcb79c580d8584ca0dd98711
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-03-14 13:45:19 +0100

    Use build.rs to add associated domain entitlements to iOS
    Add applinks and webcredentials

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-ENTITLEMNT_PATH=src-tauri/gen/apple/fds-scanner_iOS/fds-scanner_iOS.entitlements
-if ! grep -q "webcredentials" $ENTITLEMNT_PATH; then
-    # should contain:
-    # <string>webcredentials:fragdenstaat.de</string>
-    echo "ERROR: webcredentials not found in entitlements file!"
-    exit 1
-fi
-
 # Set vars from .env file
 set -a && source .env && set +a
 # Unset vars that would make tauri build
--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,23 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+    let associated_domains = [
+        "applinks:fragdenstaat.de",
+        "applinks:app.fragdenstaat.de",
+        "webcredentials:fragdenstaat.de",
+        "webcredentials:app.fragdenstaat.de",
+    ];
+    #[cfg(any(target_os = "ios", target_os = "macos"))]
+    {
+        tauri_plugin::mobile::update_entitlements(|entitlements| {
+            entitlements.insert(
+                "com.apple.developer.associated-domains".into(),
+                associated_domains
+                    .into_iter()
+                    .map(|d| d.into())
+                    .collect::<Vec<_>>()
+                    .into(),
+            );
+        })
+        .expect("failed to update entitlements");
+    }
 }
